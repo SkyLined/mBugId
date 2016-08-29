8920ab7eb1917db6d0484d0a72375ce6ccfa40f8
SkyLined <berendjanwever@gmail.com>
2016-08-29 11:38:36 +0200

    Better Windows Kits installation path detection

--- dsCdbBinaryPath_sISA.py
+++ dsCdbBinaryPath_sISA.py
@@ -18,13 +18,15 @@ if sCdb_x64:
 sProgramFilesPath_x86 = os.getenv("ProgramFiles(x86)") or os.getenv("ProgramFiles");
 sProgramFilesPath_x64 = os.getenv("ProgramW6432");
 dasPotentialCdbBinaryPaths_sISA["x86"].extend([
-  os.path.join(sProgramFilesPath_x86, "Windows Kits", "8.1", "Debuggers", "x86", "cdb.exe"),    # WDK 8.1
-  os.path.join(sProgramFilesPath_x86, "Windows Kits", "10", "Debuggers", "x86", "cdb.exe"),    # WDK 8.1
+  os.path.join(sProgramFilesPath_x86, "Windows Kits", "10", "Debuggers", "x86", "cdb.exe"),
+  os.path.join(sProgramFilesPath_x86, "Windows Kits", "8.1", "Debuggers", "x86", "cdb.exe"),
 ]);
 if sOSISA == "x64":
   dasPotentialCdbBinaryPaths_sISA["x64"].extend([
-    os.path.join(sProgramFilesPath_x64, "Windows Kits", "8.1", "Debuggers", "x64", "cdb.exe"),  # WDK 8.1
-    os.path.join(sProgramFilesPath_x64, "Windows Kits", "10", "Debuggers", "x64", "cdb.exe"),  # WDK 8.1
+    os.path.join(sProgramFilesPath_x64, "Windows Kits", "10", "Debuggers", "x64", "cdb.exe"),
+    os.path.join(sProgramFilesPath_x64, "Windows Kits", "8.1", "Debuggers", "x64", "cdb.exe"),
+    os.path.join(sProgramFilesPath_x86, "Windows Kits", "10", "Debuggers", "x64", "cdb.exe"),
+    os.path.join(sProgramFilesPath_x86, "Windows Kits", "8.1", "Debuggers", "x64", "cdb.exe"),
   ]);
 
 dsCdbBinaryPath_sISA = {};
