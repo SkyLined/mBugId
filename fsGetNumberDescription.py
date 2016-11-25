d8a44696a809e3c5e4ce26e7fa974bd54d96f2ea
SkyLined <berendjanwever@gmail.com>
2016-11-26 00:38:16 +0100

    Allow None for dxBugIdConfig["uArchitectureIndependentBugIdBits"]

--- fsGetNumberDescription.py
+++ fsGetNumberDescription.py
@@ -1,7 +1,7 @@
 from dxBugIdConfig import dxBugIdConfig;
 
 def fsGetNumberDescription(uNumber, sSign = "+"):
-  uArchitectureIndependentBugIdBytes = dxBugIdConfig["uArchitectureIndependentBugIdBits"] / 8;
+  uArchitectureIndependentBugIdBytes = (dxBugIdConfig["uArchitectureIndependentBugIdBits"] or 0) / 8;
   if uArchitectureIndependentBugIdBytes == 0 or uNumber < uArchitectureIndependentBugIdBytes:
     # Architecture independent bug ids are disabled, or the number is too small to require fixing.
     if uNumber < 10:
