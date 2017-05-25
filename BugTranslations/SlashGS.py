95be6306aa74442fd11b512f5a4276339e0be639
SkyLined <github@skylined.nl>
2017-05-25 10:25:08 +0200

    Hide /GS related functions

---
+++ BugTranslations/SlashGS.py
@@ -0,0 +1,10 @@
+from cBugTranslation import cBugTranslation;
+
+aoBugTranslations = [];
+# OOBW@Stack (hide irrelevant frames only)
+aoBugTranslations.append(cBugTranslation(
+  sOriginalBugTypeId = "OOBW@Stack",
+  asOriginalTopStackFrameSymbols = [
+    "*!__security_check_cookie",
+  ],
+));
