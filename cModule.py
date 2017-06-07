e0d0ee68d110634506e4eff158a25703d763d395
SkyLined <github@skylined.nl>
2017-06-07 17:47:47 +0200

    cModule accepts cdbid or address in constructor
    Also merged information gathering commands into one to speed up analysis

--- cModule.py
+++ cModule.py
@@ -2,16 +2,18 @@ import os, re;
 from cFunction import cFunction;
 
 class cModule(object):
-  def __init__(oModule, oProcess, sCdbId, uStartAddress = None, uEndAddress = None):
+  def __init__(oModule, oProcess, sCdbId = None, uStartAddress = None, uEndAddress = None):
     oModule.oProcess = oProcess;
-    oModule.sCdbId = sCdbId;
-    # oModule.uStartAddress is not always known from the start and may only be determined when needed using __fGetAddressesAndSymbolsStatus.
+    # oModule.sCdbId is not always known from the start and may only be determined when needed using __fGetModuleInformation.
+    oModule.__sCdbId = sCdbId;
+    # oModule.uStartAddress is not always known from the start and may only be determined when needed using __fGetModuleInformation.
     oModule.__uStartAddress = uStartAddress;
-    # oModule.uEndAddress is not always known from the start and may only be determined when needed using __fGetAddressesAndSymbolsStatus.
+    # oModule.uEndAddress is not always known from the start and may only be determined when needed using __fGetModuleInformation.
     oModule.__uEndAddress = uEndAddress;
-    # oModule.bSymbolsAvailable is only determined when needed using __fGetAddressesAndSymbolsStatus
+    # oModule.bSymbolsAvailable is only determined when needed using __fGetModuleInformation
     oModule.__bSymbolsAvailable = None;
-    oModule.__bSymbolsStatusKnown = False;
+    # __fGetModuleInformation needs only be called once:
+    oModule.__bModuleInformationAvailable = False; # set to false when __fGetModuleInformation is called.
     oModule.__sBinaryName = None;
     oModule.__sFileVersion = None;
     oModule.__sTimestamp = None;
@@ -24,82 +26,57 @@ class cModule(object):
       oModule.__doFunction_by_sSymbol[sSymbol] = cFunction(oModule, sSymbol);
     return oModule.__doFunction_by_sSymbol[sSymbol];
   
+  # The below three may have been provided in the constructors arguments, so we may not need to call __fGetModuleInformation.
+  @property
+  def sCdbId(oModule):
+    if oModule.__sCdbId is None:
+      oModule.__fGetModuleInformation();
+    return oModule.__sCdbId;
+  
   @property
   def uStartAddress(oModule):
     if oModule.__uStartAddress is None:
-      oModule.__fGetAddressesAndSymbolsStatus();
+      oModule.__fGetModuleInformation();
     return oModule.__uStartAddress;
   
   @property
   def uEndAddress(oModule):
     if oModule.__uEndAddress is None:
-      oModule.__fGetAddressesAndSymbolsStatus();
+      oModule.__fGetModuleInformation();
     return oModule.__uEndAddress;
   
+  # The below are never available until __fGetModuleInformation is called:
   @property
   def bSymbolsAvailable(oModule):
-    if not oModule.__bSymbolsStatusKnown:
-      # Find out the symbols status
-      oModule.__fGetAddressesAndSymbolsStatus();
+    if not oModule.__bModuleInformationAvailable:
+      oModule.__fGetModuleInformation();
+    # Find out the symbols status
     if oModule.__bSymbolsAvailable is None:
       # It's deferred: try to load symbols
       asLoadSymbolsOutput = oModule.oProcess.fasExecuteCdbCommand("ld %s; $$ Load symbols for module" % oModule.sCdbId);
       assert len(asLoadSymbolsOutput) == 1 and re.match(r"Symbols (already )?loaded for %s" % oModule.sCdbId, asLoadSymbolsOutput[0]), \
           "Unexpected load symbols output:\r\n%s" % "\r\n".join(asLoadSymbolsOutput);
-      # Unfortunately, it does not tell us if it loadad a pdb, or if export symbols are used.
-      oModule.__fGetAddressesAndSymbolsStatus();
+      # Unfortunately, it does not tell us if it loaded a pdb, or if export symbols are used.
+      # So we will call __fGetModuleInformation again to find out.
+      oModule.__fGetModuleInformation();
     return oModule.__bSymbolsAvailable;
-  
-  def __fGetAddressesAndSymbolsStatus(oModule):
-    asListModuleOutput = oModule.oProcess.fasExecuteCdbCommand("lmo m %s; $$ List addresses for module" % oModule.sCdbId);
-    assert len(asListModuleOutput) == 2, \
-        "Expected 2 lines of output, got %d:\r\n%s" % (len(asListModuleOutput), "\r\n".join(asListModuleOutput));
-    oModule.__fParseListModuleFirstLines(asListModuleOutput);
-  
-  def __fParseListModuleFirstLines(oModule, asListModuleOutput):
-    assert re.match("^start\s+end\s+module name\s*$", asListModuleOutput[0]), \
-        "Unexpected list module output on line 1:\r\n%s" % "\r\n".join(asListModuleOutput);
-    oAddressesAndSymbolStatusMatch = re.match(
-      r"^\s*%s\s*$" % "\s+".join([
-        r"([0-9a-f`]+)",                              # (start_address)
-        r"([0-9a-f`]+)",                              # (end_address)
-        r"(\w+)(?:\s+C)?",                            # (cdb_module_id) [ space "C" ] # not sure what this "C" means
-        r"\((deferred|(?:export|no|(?:private )?pdb) symbols)\)", # "(" symbol status ")"
-        r".*?",                                       # symbol information.
-      ]),
-      asListModuleOutput[1],
-      re.I
-    );
-    assert oAddressesAndSymbolStatusMatch, \
-        "Unexpected list module output on line 2:\r\n%s" % "\r\n".join(asListModuleOutput);
-    (sStartAddress, sEndAddress, sCdbId, sSymbolStatus) = oAddressesAndSymbolStatusMatch.groups();
-    oModule.__uStartAddress = long(sStartAddress.replace("`", ""), 16);
-    oModule.__uEndAddress = long(sEndAddress.replace("`", ""), 16);
-    oModule.__bSymbolsAvailable = {
-      "deferred": None,
-      "export symbols": False,
-      "no symbols": False,
-      "pdb symbols": True,
-      "private pdb symbols": True,
-    }[sSymbolStatus];
-    oModule.__bSymbolsStatusKnown = True;
   
   @property
   def sBinaryName(oModule):
-    if oModule.__sTimestamp is None: # sTimeStamp is always set by __fGetBinaryInformation
-      oModule.__fGetBinaryInformation();
+    if not oModule.__bModuleInformationAvailable:
+      oModule.__fGetModuleInformation();
     return oModule.__sBinaryName;
   
   @property
   def sFileVersion(oModule):
-    if oModule.__sTimestamp is None: # sTimeStamp is always set by __fGetBinaryInformation
-      oModule.__fGetBinaryInformation();
+    if not oModule.__bModuleInformationAvailable:
+      oModule.__fGetModuleInformation();
     return oModule.__sFileVersion;
   @property
   
   def sTimestamp(oModule):
-    if oModule.__sTimestamp is None:
-      oModule.__fGetBinaryInformation();
+    if not oModule.__bModuleInformationAvailable:
+      oModule.__fGetModuleInformation();
     return oModule.__sTimestamp;
   
   @property
@@ -112,11 +89,11 @@ class cModule(object):
   
   @property
   def sInformationHTML(oModule):
-    if oModule.__sInformationHTML is None:
-      oModule.__fGetBinaryInformation();
+    if not oModule.__bModuleInformationAvailable:
+      oModule.__fGetModuleInformation();
     return oModule.__sInformationHTML;
   
-  def __fGetBinaryInformation(oModule):
+  def __fGetModuleInformation(oModule):
     # TODO: The HTML report output should really not be produced here (aka push the data). Rather, it should be created
     # on demand using another function call when the report is created (aka pull the data). This would also do away
     # with all references to oCdbWrapper
@@ -125,7 +102,8 @@ class cModule(object):
     # Also sets oModule.sFileVersion if possible.
     oCdbWrapper = oModule.oProcess.oCdbWrapper;
     asListModuleOutput = oModule.oProcess.fasExecuteCdbCommand(
-      "lmv m %s; $$ Get module information" % oModule.sCdbId,
+        oModule.__sCdbId and "lmov m %s; $$ Get module information" % oModule.__sCdbId
+        or "lmov a 0x%X; $$ Get module information" % oModule.uStartAddress,
       bOutputIsInformative = True,
     );
     # Sample output:
@@ -134,6 +112,7 @@ class cModule(object):
     # |00000000`011b0000 00000000`0120f000   firefox    (deferred)             
     # |    Image path: firefox.exe
     # |    Image name: firefox.exe
+    # |    Image was built with /Brepro flag.
     # |    Timestamp:        Thu Aug 13 03:23:30 2015 (55CBF192)
     # |    CheckSum:         0006133B
     # |    ImageSize:        0005F000
@@ -159,18 +138,56 @@ class cModule(object):
       asModuleInformationTableRowsHTML = [];
     assert len(asListModuleOutput) > 2, \
         "Expected at least 3 lines of list module output, got %d:\r\n%s" % (len(asListModuleOutput), "\r\n".join(asListModuleOutput));
-    oModule.__fParseListModuleFirstLines(asListModuleOutput);
+    assert re.match("^start\s+end\s+module name\s*$", asListModuleOutput[0]), \
+        "Unexpected list module output on line 1:\r\n%s" % "\r\n".join(asListModuleOutput);
+    oAddressesAndSymbolStatusMatch = re.match(
+      r"^\s*%s\s*$" % "\s+".join([
+        r"([0-9a-f`]+)",                              # (start_address)
+        r"([0-9a-f`]+)",                              # (end_address)
+        r"(\w+)(?:\s+C)?",                            # (cdb_module_id) [ space "C" ] # not sure what this "C" means
+        r"\((deferred|(?:export|no|(?:private )?pdb) symbols)\)", # "(" symbol status ")"
+        r".*?",                                       # symbol information.
+      ]),
+      asListModuleOutput[1],
+      re.I
+    );
+    assert oAddressesAndSymbolStatusMatch, \
+        "Unexpected list module output on line 2:\r\n%s" % "\r\n".join(asListModuleOutput);
+    (sStartAddress, sEndAddress, sCdbId, sSymbolStatus) = oAddressesAndSymbolStatusMatch.groups();
+    uStartAddress = long(sStartAddress.replace("`", ""), 16);
+    uEndAddress = long(sEndAddress.replace("`", ""), 16);
+    assert oModule.__uStartAddress in [None, uStartAddress], \
+        "Module start address was given as 0x%X, but is now reported to be 0x%X" % (oModule.__uStartAddress, uStartAddress);
+    assert oModule.__uEndAddress in [None, uEndAddress], \
+        "Module end address was given as 0x%X, but is now reported to be 0x%X" % (oModule.__uEndAddress, uEndAddress);
+    assert oModule.__sCdbId in [None, sCdbId], \
+        "Module cdb id was given as %s, but is now reported to be %s" % (repr(oModule.__sCdbId), repr(sCdbId));
+    oModule.__uStartAddress = uStartAddress;
+    oModule.__uEndAddress = uEndAddress;
+    oModule.__sCdbId = sCdbId;
+    oModule.__bSymbolsAvailable = {
+      "deferred": None,
+      "export symbols": False,
+      "no symbols": False,
+      "pdb symbols": True,
+      "private pdb symbols": True,
+    }[sSymbolStatus];
+    
     dsValue_by_sName = {};
     for sLine in asListModuleOutput[2:]:
-      oNameAndValueMatch = re.match(r"^\s+([^:]+):\s+(.*?)\s*$", sLine);
-      assert oNameAndValueMatch, \
-          "Unexpected list module output: %s\r\n%s" % (sLine, "\r\n".join(asListModuleOutput));
-      (sName, sValue) = oNameAndValueMatch.groups();
-      dsValue_by_sName[sName] = sValue;
-      if oCdbWrapper.bGenerateReportHTML:
-        asModuleInformationTableRowsHTML.append(
-          '<tr><td>%s</td><td>%s</td></tr>' % (oCdbWrapper.fsHTMLEncode(sName), oCdbWrapper.fsHTMLEncode(sValue)),
-        );
+      # These lines different from the "name: value" format and handled separately:
+      if sLine == "    Image was built with /Brepro flag.":
+        continue; # Ignored.
+      else:
+        oNameAndValueMatch = re.match(r"^\s+([^:]+):\s+(.*?)\s*$", sLine);
+        assert oNameAndValueMatch, \
+            "Unexpected list module output: %s\r\n%s" % (sLine, "\r\n".join(asListModuleOutput));
+        (sName, sValue) = oNameAndValueMatch.groups();
+        dsValue_by_sName[sName] = sValue;
+        if oCdbWrapper.bGenerateReportHTML:
+          asModuleInformationTableRowsHTML.append(
+            '<tr><td>%s</td><td>%s</td></tr>' % (oCdbWrapper.fsHTMLEncode(sName), oCdbWrapper.fsHTMLEncode(sValue)),
+          );
     oModule.__sBinaryName = "Image path" in dsValue_by_sName and os.path.basename(dsValue_by_sName["Image path"]) or None;
     oModule.__sFileVersion = dsValue_by_sName.get("File version");
     oModule.__sTimestamp = dsValue_by_sName["Timestamp"];
