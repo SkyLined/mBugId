3a1e022ce47b93118503c30b4a948cf4e6363dbe
SkyLined <github@skylined.nl>
2017-06-19 22:15:59 +0200

    Update magic values

--- cBugReport_foAnalyzeException_STATUS_ACCESS_VIOLATION.py
+++ cBugReport_foAnalyzeException_STATUS_ACCESS_VIOLATION.py
@@ -10,37 +10,64 @@ from sBlockHTMLTemplate import sBlockHTMLTemplate;
 
 ddtsDetails_uSpecialAddress_sISA = {
   "x86": {              # Id                      Description                                           Security impact
-            0x00000000: ('NULL',                  "a NULL ptr",                                         None),
-            0xBBADBEEF: ('Assertion',             "an address that indicates an assertion has failed",  None),
-            0xBAADF00D: ('PoisonUninitialized',   "a pointer that was not initialized",                 "Potentially exploitable security issue"),
-            0xCCCCCCCC: ('PoisonUninitialized',   "a pointer that was not initialized",                 "Potentially exploitable security issue"),
-            0xC0C0C0C0: ('PoisonUninitialized',   "a pointer that was not initialized",                 "Potentially exploitable security issue"),
-            0xCDCDCDCD: ('PoisonUninitialized',   "a pointer that was not initialized",                 "Potentially exploitable security issue"),
-            0xD0D0D0D0: ('PoisonUninitialized',   "a pointer that was not initialized",                 "Potentially exploitable security issue"),
-            0xDDDDDDDD: ('PoisonFree',            "a pointer read from poisoned freed memory",          "Potentially exploitable security issue"),
-            0xF0F0F0F0: ('PoisonFree',            "a pointer read from poisoned freed memory",          "Potentially exploitable security issue"),
-            0xFDFDFDFD: ('PoisonOOB',             "a pointer read from an out-of-bounds memory canary", "Potentially exploitable security issue"),
-            0xF0DE7FFF: ('Poison',                "a pointer read from poisoned memory",                "Potentially exploitable security issue"),
-            0xF0090100: ('Poison',                "a pointer read from poisoned memory",                "Potentially exploitable security issue"),
-            0xFEEEFEEE: ('PoisonFree',            "a pointer read from poisoned freed memory",          "Potentially exploitable security issue"),
+            # https://en.wikipedia.org/wiki/Magic_number_(programming)#Magic_debug_values
+            0x00000000: ("NULL",                  "a NULL ptr",                                         None),
+            
+            0xA0A0A0A0: ("PoisonOOB",             "a pointer read from an out-of-bounds memory canary", "Potentially exploitable security issue"),       
+            # https://msdn.microsoft.com/en-us/library/ms220938(v=vs.90).aspx
+            0xABCDAAAA: ("PoisonOOB",             "a pointer read from an out-of-bounds memory canary in a heap block header", "Potentially exploitable security issue"),
+            0xABCDAAA9: ("PoisonOOB",             "a pointer read from an out-of-bounds memory canary in a freed heap block header", "Potentially exploitable security issue"),
+            0xABCDBBBA: ("PoisonOOB",             "a pointer read from an out-of-bounds memory canary in a freed full heap block header", "Potentially exploitable security issue"),   
+            0xABCDBBBB: ("PoisonOOB",             "a pointer read from an out-of-bounds memory canary in a full heap block header", "Potentially exploitable security issue"),       
+            
+            0xBBADBEEF: ("Assertion",             "an address that indicates an assertion has failed",  None),
+            
+            0xBAADF00D: ("PoisonUninitialized",   "a pointer that was not initialized",                 "Potentially exploitable security issue"),
+            
+            0xCCCCCCCC: ("PoisonUninitialized",   "a pointer that was not initialized",                 "Potentially exploitable security issue"),
+            
+            0xC0C0C0C0: ("PoisonUninitialized",   "a pointer that was not initialized",                 "Potentially exploitable security issue"),
+            
+            0xCDCDCDCD: ("PoisonUninitialized",   "a pointer that was not initialized",                 "Potentially exploitable security issue"),
+            
+            0xD0D0D0D0: ("PoisonUninitialized",   "a pointer that was not initialized",                 "Potentially exploitable security issue"),
+            # https://msdn.microsoft.com/en-us/library/ms220938(v=vs.90).aspx
+            0xDCBAAAA9: ("PoisonOOB",             "a pointer read from an out-of-bounds memory canary in a heap block header", "Potentially exploitable security issue"),
+            0xDCBAAAAA: ("PoisonOOB",             "a pointer read from an out-of-bounds memory canary in a freed heap block header", "Potentially exploitable security issue"),
+            0xDCBABBBA: ("PoisonOOB",             "a pointer read from an out-of-bounds memory canary in a freed heap block header", "Potentially exploitable security issue"),
+            0xDCBABBBB: ("PoisonOOB",             "a pointer read from an out-of-bounds memory canary in a heap block header", "Potentially exploitable security issue"),
+
+            0xDDDDDDDD: ("PoisonFree",            "a pointer read from poisoned freed memory",          "Potentially exploitable security issue"),
+            
+            0xE0E0E0E0: ("PoisonUninitialized",   "a pointer that was not initialized",                 "Potentially exploitable security issue"),             
             # https://hg.mozilla.org/releases/mozilla-beta/rev/8008235a2429
-            0XE4E4E4E4: ("PoisonUninitialized",   "a pointer that was not initialized",                 "Potentially exploitable security issue"),
-            0XE5E5E5E5: ("PoisonFree",            "a pointer read from poisoned freed memory",          "Potentially exploitable security issue"),
+            0xE4E4E4E4: ("PoisonUninitialized",   "a pointer that was not initialized",                 "Potentially exploitable security issue"),
+            0xE5E5E5E5: ("PoisonFree",            "a pointer read from poisoned freed memory",          "Potentially exploitable security issue"),
+            
+            0xF0090100: ("Poison",                "a pointer read from poisoned memory",                "Potentially exploitable security issue"),
+            
+            0xF0DE7FFF: ("Poison",                "a pointer read from poisoned memory",                "Potentially exploitable security issue"),
+            
+            0xF0F0F0F0: ("PoisonFree",            "a pointer read from poisoned freed memory",          "Potentially exploitable security issue"),
+            
+            0xFDFDFDFD: ("PoisonOOB",             "a pointer read from an out-of-bounds memory canary", "Potentially exploitable security issue"),
+            
+            0xFEEEFEEE: ("PoisonFree",            "a pointer read from poisoned freed memory",          "Potentially exploitable security issue"),
   },
   "x64": {              # Id                      Description                                           Security impact
-    0x0000000000000000: ('NULL',                  "a NULL ptr",                                         None),
+    0x0000000000000000: ("NULL",                  "a NULL ptr",                                         None),
     # Note that on x64, addresses with the most significant bit set cannot be allocated in user-land. Since BugId is expected to analyze only user-land
     # applications, accessing such an address is not expected to be an exploitable security issue.
-    0xBAADF00DBAADF00D: ('PoisonUninitialized',   "a pointer that was not initialized",                 None),
-    0xCCCCCCCCCCCCCCCC: ('PoisonUninitialized',   "a pointer that was not initialized",                 None),
-    0xC0C0C0C0C0C0C0C0: ('PoisonUninitialized',   "a pointer that was not initialized",                 None),
-    0xCDCDCDCDCDCDCDCD: ('PoisonUninitialized',   "a pointer that was not initialized",                 None),
-    0xD0D0D0D0D0D0D0D0: ('PoisonUninitialized',   "a pointer that was not initialized",                 None),
-    0xDDDDDDDDDDDDDDDD: ('PoisonFree',            "a pointer read from poisoned freed memory",          None),
-    0xF0F0F0F0F0F0F0F0: ('PoisonFree',            "a pointer read from poisoned freed memory",          None),
-    0xF0DE7FFFF0DE7FFF: ('Poison',                "a pointer read from poisoned memory",                None),
-    0xF0090100F0090100: ('Poison',                "a pointer read from poisoned memory",                None),
-    0xFEEEFEEEFEEEFEEE: ('PoisonFree',            "a pointer read from poisoned freed memory",          None),
+    0xBAADF00DBAADF00D: ("PoisonUninitialized",   "a pointer that was not initialized",                 None),
+    0xCCCCCCCCCCCCCCCC: ("PoisonUninitialized",   "a pointer that was not initialized",                 None),
+    0xC0C0C0C0C0C0C0C0: ("PoisonUninitialized",   "a pointer that was not initialized",                 None),
+    0xCDCDCDCDCDCDCDCD: ("PoisonUninitialized",   "a pointer that was not initialized",                 None),
+    0xD0D0D0D0D0D0D0D0: ("PoisonUninitialized",   "a pointer that was not initialized",                 None),
+    0xDDDDDDDDDDDDDDDD: ("PoisonFree",            "a pointer read from poisoned freed memory",          None),
+    0xF0F0F0F0F0F0F0F0: ("PoisonFree",            "a pointer read from poisoned freed memory",          None),
+    0xF0DE7FFFF0DE7FFF: ("Poison",                "a pointer read from poisoned memory",                None),
+    0xF0090100F0090100: ("Poison",                "a pointer read from poisoned memory",                None),
+    0xFEEEFEEEFEEEFEEE: ("PoisonFree",            "a pointer read from poisoned freed memory",          None),
     # https://hg.mozilla.org/releases/mozilla-beta/rev/8008235a2429
     0xE4E4E4E4E4E4E4E4: ("PoisonUninitialized",   "a pointer that was not initialized",                 None),
     0xE5E5E5E5E5E5E5E5: ("PoisonFree",            "a pointer read from poisoned freed memory",          None),
