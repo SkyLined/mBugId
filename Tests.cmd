6a2dfde249a0412b972de1e2f6f66e1b3967c205
SkyLined <github@skylined.nl>
2017-06-19 22:48:55 +0200

    Check for PYTHON before running tests

--- Tests.cmd
+++ Tests.cmd
@@ -1,6 +1,25 @@
 @ECHO OFF
-SET _NT_SYMBOL_PATH=
+:: Try to find out where python is installed if it is not provided.
+IF NOT DEFINED PYTHON (
+  :: See if it's found anywhere in the PATH
+  FOR /F "usebackq delims=/" %%I IN (`where python 2^>nul`) DO (
+    SET PYTHON=%%I
+  )
+)
+IF NOT DEFINED PYTHON (
+  SET PYTHON="%SystemDrive%\Python27\python.exe"
+) ELSE (
+  SET PYTHON="%PYTHON:"=%"
+)
+:: Make sure python can be found
+IF NOT EXIST %PYTHON% (
+  ECHO - Cannot find Python at %PYTHON%, please set the "PYTHON" environment variable to the correct path.
+  EXIT /B 1
+)
+:: If the user wants to run a full test-suite, new reports will be generated so the old ones must be deleted so as to
+:: not leave old reports intermingled with the new ones.
 IF "%~1" == "--full" (
   DEL "Tests\Reports\*" /Q
 )
-PYTHON Tests\Tests.py %*
+:: Run tests
+%PYTHON% Tests\Tests.py %*
