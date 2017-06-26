f81adffc55c9e05b4cd875a221260f7018d03263
SkyLined <github@skylined.nl>
2017-06-26 11:02:30 +0200

    Improve V8 bug translations

--- BugTranslations/V8.py
+++ BugTranslations/V8.py
@@ -1,38 +1,54 @@
 import re;
 from cBugTranslation import cBugTranslation;
 
-aoBugTranslations = [];
+aoBugTranslations = [
 # AVE@NULL -> OOM
-aoBugTranslations.append(cBugTranslation(
-  sOriginalBugTypeId = "AVE@NULL",
-  asOriginalTopStackFrameSymbols = [
-    "0x0",
-    "*!v8::base::OS::Abort",
-    "*!v8::Utils::ReportApiFailure",
-    "*!v8::Utils::ApiCheck",
-    "*!v8::internal::V8::FatalProcessOutOfMemory",
-  ],
-  sTranslatedBugTypeId = "OOM",
-  sTranslatedBugDescription = "The application caused an access violation by calling NULL to indicate it was unable to allocate enough memory.",
-  sTranslatedSecurityImpact = None,
-));
-# Assert -> OOM
-aoBugTranslations.append(cBugTranslation(
-  sOriginalBugTypeId = "Assert",
-  asOriginalTopStackFrameSymbols = [
-    "*!v8::internal::V8::FatalProcessOutOfMemory",
-  ],
-  sTranslatedBugTypeId = "OOM",
-  sTranslatedBugDescription = "The application triggered a breakpoint to indicate it was unable to allocate enough memory.",
-  sTranslatedSecurityImpact = None,
-));
-# AVE@NULL -> Assert
-aoBugTranslations.append(cBugTranslation(
-  sOriginalBugTypeId = "AVE@NULL",
-  asOriginalTopStackFrameSymbols = [
-    "*!V8_Fatal",
-  ],
-  sTranslatedBugTypeId = "Assert",
-  sTranslatedBugDescription = "The application caused an access violation by calling NULL to indicate an assertion failed.",
-  sTranslatedSecurityImpact = None,
-));
+  cBugTranslation(
+    sOriginalBugTypeId = "AVE@NULL",
+    asOriginalTopStackFrameSymbols = [
+      "0x0",
+      "*!v8::base::OS::Abort",
+      "*!v8::Utils::ReportApiFailure",
+      "*!v8::Utils::ApiCheck",
+      "*!v8::internal::V8::FatalProcessOutOfMemory",
+    ],
+    sTranslatedBugTypeId = "OOM",
+    sTranslatedBugDescription = "The application caused an access violation by calling NULL to indicate it was unable to allocate enough memory.",
+    sTranslatedSecurityImpact = None,
+  ),
+  # Assert -> OOM
+  cBugTranslation(
+    sOriginalBugTypeId = "Assert",
+    asOriginalTopStackFrameSymbols = [
+      "*!v8::internal::V8::FatalProcessOutOfMemory",
+    ],
+    sTranslatedBugTypeId = "OOM",
+    sTranslatedBugDescription = "The application triggered a breakpoint to indicate it was unable to allocate enough memory.",
+    sTranslatedSecurityImpact = None,
+  ),
+  # AVE@NULL -> Assert
+  cBugTranslation(
+    sOriginalBugTypeId = "AVE@NULL",
+    asOriginalTopStackFrameSymbols = [
+      "*!V8_Fatal",
+    ],
+    sTranslatedBugTypeId = "Assert",
+    sTranslatedBugDescription = "The application caused an access violation by calling NULL to indicate an assertion failed.",
+    sTranslatedSecurityImpact = None,
+  ),
+  # Assert -> ignore functions
+  cBugTranslation(
+    sOriginalBugTypeId = "Assert",
+    aasAdditionalIrrelevantStackFrameSymbols = [
+      [
+        "*!v8::Utils::ReportApiFailure",
+      ], [
+        "*!v8::Utils::ApiCheck",
+      ], [
+        "*!v8::V8::ToLocalEmpty",
+      ], [
+        "*!v8::MaybeLocal",
+      ],
+    ],
+  ),
+];
