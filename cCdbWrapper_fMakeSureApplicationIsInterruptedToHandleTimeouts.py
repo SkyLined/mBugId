43b2502b67bba2990fbdd8ab3eba43285913bd63
SkyLined <github@skylined.nl>
2017-07-31 16:59:45 +0200

    Only interrupt the application once while it is running

--- cCdbWrapper_fMakeSureApplicationIsInterruptedToHandleTimeouts.py
+++ cCdbWrapper_fMakeSureApplicationIsInterruptedToHandleTimeouts.py
@@ -2,8 +2,9 @@ import signal, time;
 from WindowsAPI import *;
 
 def cCdbWrapper_fMakeSureApplicationIsInterruptedToHandleTimeouts(oCdbWrapper):
-  # Let the StdIO thread know a break exception was sent so it knows to expected cdb to report one (otherwise
-  # it would get reported as a bug!).
-  oCdbWrapper.uCdbBreakExceptionsPending += 1;
-  assert KERNEL32.GenerateConsoleCtrlEvent(CTRL_BREAK_EVENT, oCdbWrapper.oCdbProcess.pid), \
-      "Failed to send CTRL+BREAK to cdb process with id %d/0x%X" % (oCdbWrapper.oCdbProcess.pid, oCdbWrapper.oCdbProcess.pid);
+  if oCdbWrapper.uCdbBreakExceptionsPending == 0:
+    # Let the StdIO thread know a break exception was sent so it knows to expected cdb to report one (otherwise
+    # it would get reported as a bug!).
+    oCdbWrapper.uCdbBreakExceptionsPending += 1;
+    assert KERNEL32.GenerateConsoleCtrlEvent(CTRL_BREAK_EVENT, oCdbWrapper.oCdbProcess.pid), \
+        "Failed to send CTRL+BREAK to cdb process with id %d/0x%X" % (oCdbWrapper.oCdbProcess.pid, oCdbWrapper.oCdbProcess.pid);
