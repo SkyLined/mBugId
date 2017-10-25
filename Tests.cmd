6109f4d6bea01dca1d67a2fee37fe326c86a4eb6
SkyLined <github@skylined.nl>
2017-10-25 08:53:33 +0200

    Update Tests.cmd script

--- Tests.cmd
+++ Tests.cmd
@@ -1,25 +1,33 @@
 @ECHO OFF
-:: Try to find out where python is installed if it is not provided.
-IF NOT DEFINED PYTHON (
-  :: See if it's found anywhere in the PATH
-  FOR /F "usebackq delims=/" %%I IN (`where python 2^>nul`) DO (
-    SET PYTHON=%%I
+SETLOCAL
+IF DEFINED PYTHON (
+  CALL :CHECK_PYTHON
+  IF ERRORLEVEL 1 EXIT /B 1
+) ELSE (
+  REM Try to detect the location of python automatically
+  FOR /F "usebackq delims=" %%I IN (`where "python" 2^>nul`) DO SET PYTHON="%%~I"
+  IF NOT DEFINED PYTHON (
+    REM Check if python is found in its default installation path.
+    SET PYTHON="%SystemDrive%\Python27\python.exe"
+    CALL :CHECK_PYTHON
+    IF ERRORLEVEL 1 EXIT /B 1
   )
 )
-IF NOT DEFINED PYTHON (
-  SET PYTHON="%SystemDrive%\Python27\python.exe"
-) ELSE (
-  SET PYTHON="%PYTHON:"=%"
-)
-:: Make sure python can be found
-IF NOT EXIST %PYTHON% (
-  ECHO - Cannot find Python at %PYTHON%, please set the "PYTHON" environment variable to the correct path.
-  EXIT /B 1
-)
+
 :: If the user wants to run a full test-suite, new reports will be generated so the old ones must be deleted so as to
 :: not leave old reports intermingled with the new ones.
 IF "%~1" == "--full" (
   DEL "Tests\Reports\*" /Q
 )
-:: Run tests
-%PYTHON% Tests\Tests.py %*
+SET _NT_SYMBOL_PATH=
+%PYTHON% "%~dpn0\%~n0.py" %*
+ENDLOCAL & EXIT /B %ERRORLEVEL%
+
+:CHECK_PYTHON
+  REM Make sure path is quoted and check if it exists.
+  SET PYTHON="%PYTHON:"=%"
+  IF NOT EXIST %PYTHON% (
+    ECHO - Cannot find Python at %PYTHON%, please set the "PYTHON" environment variable to the correct path.
+    EXIT /B 1
+  )
+  EXIT /B 0
