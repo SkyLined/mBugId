02787635630c8a755570223b294382077bd7d705
SkyLined <github@skylined.nl>
2017-11-28 20:30:11 +0100

    wil bug translations update

--- BugTranslations/wil.py
+++ BugTranslations/wil.py
@@ -10,16 +10,21 @@ aoBugTranslations = [
       ],
     ],
   ),
-  # AppExit -> OOM
+  # Breakpoint -> Assert
   cBugTranslation(
-    sOriginalBugTypeId = "AppExit",
+    sOriginalBugTypeId = "Breakpoint",
     asOriginalTopStackFrameSymbols = [
       "*!wil::details::ReportFailure",
-      "*!wil::details::ReportFailure_Hr",
-      "*!wil::details::in1diag3::_FailFast_NullAlloc",
     ],
-    sTranslatedBugTypeId = "OOM",
-    sTranslatedBugDescription = "The application triggered a fail fast application exit to indicate it was unable to allocate enough memory.",
+    aasAdditionalIrrelevantStackFrameSymbols = [
+      [
+        "*!wil::details::ReportFailure_Win32",
+      ], [
+        "*!wil::details::in1diag3::_FailFast_Win32",
+      ],
+    ],
+    sTranslatedBugTypeId = "Assert:Breakpoint",
+    sTranslatedBugDescription = "The application triggered a debugger breakpoint to indicate an assertion failed.",
     sTranslatedSecurityImpact = "Unlikely to be exploitable, unless you can find a way to avoid this breakpoint.",
   ),
   # AppExit -> Assert
@@ -27,17 +32,38 @@ aoBugTranslations = [
     sOriginalBugTypeId = "AppExit",
     asOriginalTopStackFrameSymbols = [
       "*!wil::details::ReportFailure",
-      "*!wil::details::ReportFailure_Hr",
     ],
     aasAdditionalIrrelevantStackFrameSymbols = [
       [
+        "*!wil::details::ReportFailure_Hr",
+      ], [
         "*!wil::details::in1diag3::FailFast_Hr",
       ], [
         "*!wil::details::in1diag3::FailFast_Unexpected",
       ],
     ],
-    sTranslatedBugTypeId = "Assert",
+    sTranslatedBugTypeId = "Assert:AppExit",
     sTranslatedBugDescription = "The application triggered a fail fast application exit to indicate an assertion failed.",
+    sTranslatedSecurityImpact = "Unlikely to be exploitable, unless you can find a way to avoid this breakpoint.",
+  ),
+  # Assert:Breakpoint -> OOM
+  cBugTranslation(
+    sOriginalBugTypeId = "Assert:AppExit",
+    asOriginalTopStackFrameSymbols = [
+      "*!wil::details::in1diag3::_FailFast_NullAlloc",
+    ],
+    sTranslatedBugTypeId = "OOM",
+    sTranslatedBugDescription = "The application triggered a debugger breakpoint to indicate it was unable to allocate enough memory.",
+    sTranslatedSecurityImpact = "Unlikely to be exploitable, unless you can find a way to avoid this breakpoint.",
+  ),
+  # Assert:AppExit -> OOM
+  cBugTranslation(
+    sOriginalBugTypeId = "Assert:AppExit",
+    asOriginalTopStackFrameSymbols = [
+      "*!wil::details::in1diag3::_FailFast_NullAlloc",
+    ],
+    sTranslatedBugTypeId = "OOM",
+    sTranslatedBugDescription = "The application triggered a fail fast application exit to indicate it was unable to allocate enough memory.",
     sTranslatedSecurityImpact = "Unlikely to be exploitable, unless you can find a way to avoid this breakpoint.",
   ),
 ];
