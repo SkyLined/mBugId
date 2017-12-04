f8da3224d93355dd284e2e9a0d866c9d51e50a72
SkyLined <github@skylined.nl>
2017-12-04 23:10:54 +0100

    wil bug translations updated

--- BugTranslations/wil.py
+++ BugTranslations/wil.py
@@ -35,20 +35,18 @@ aoBugTranslations = [
     ],
     aasAdditionalIrrelevantStackFrameSymbols = [
       [
-        "*!wil::details::ReportFailure_Hr",
+        "*!wil::details::ReportFailure_Win32",
       ], [
-        "*!wil::details::in1diag3::FailFast_Hr",
-      ], [
-        "*!wil::details::in1diag3::FailFast_Unexpected",
+        "*!wil::details::in1diag3::_FailFast_Win32",
       ],
     ],
     sTranslatedBugTypeId = "Assert:AppExit",
     sTranslatedBugDescription = "The application triggered a fail fast application exit to indicate an assertion failed.",
-    sTranslatedSecurityImpact = "Unlikely to be exploitable, unless you can find a way to avoid this breakpoint.",
+    sTranslatedSecurityImpact = "Unlikely to be exploitable, unless you can find a way to avoid this fail fast application exit.",
   ),
   # Assert:Breakpoint -> OOM
   cBugTranslation(
-    sOriginalBugTypeId = "Assert:AppExit",
+    sOriginalBugTypeId = "Assert:Breakpoint",
     asOriginalTopStackFrameSymbols = [
       "*!wil::details::in1diag3::_FailFast_NullAlloc",
     ],
@@ -64,6 +62,58 @@ aoBugTranslations = [
     ],
     sTranslatedBugTypeId = "OOM",
     sTranslatedBugDescription = "The application triggered a fail fast application exit to indicate it was unable to allocate enough memory.",
-    sTranslatedSecurityImpact = "Unlikely to be exploitable, unless you can find a way to avoid this breakpoint.",
+    sTranslatedSecurityImpact = "Unlikely to be exploitable, unless you can find a way to avoid this fail fast application exit.",
+  ),
+  # Assert:AppExit -> Assert:HRESULT
+  cBugTranslation(
+    sOriginalBugTypeId = "Assert:AppExit",
+    aasOriginalTopStackFrameSymbols = [
+      [
+        "*!wil::details::ReportFailure_Hr",
+      ], [
+        "*!wil::details::in1diag3::FailFast_Hr",
+      ], [
+        "*!wil::details::in1diag3::_FailFast_Hr",
+      ],
+    ],
+    sTranslatedBugTypeId = "Assert:HRESULT",
+    sTranslatedBugDescription = "The application triggered a fail fast application exit to indicate an assertion failed.",
+    sTranslatedSecurityImpact = "Unlikely to be exploitable, unless you can find a way to avoid this fail fast application exit.",
+  ),
+  # Assert:Breakpoint -> Assert:HRESULT
+  cBugTranslation(
+    sOriginalBugTypeId = "Assert:Breakpoint",
+    aasOriginalTopStackFrameSymbols = [
+      [
+        "*!wil::details::ReportFailure_Hr",
+      ], [
+        "*!wil::details::in1diag3::FailFast_Hr",
+      ], [
+        "*!wil::details::in1diag3::_FailFast_Hr",
+      ],
+    ],
+    sTranslatedBugTypeId = "Assert:HRESULT",
+    sTranslatedBugDescription = "The application triggered a breakpoint exception to indicate an assertion failed.",
+    sTranslatedSecurityImpact = "Unlikely to be exploitable, unless you can find a way to avoid this breakpoint exception.",
+  ),
+  # Assert:AppExit -> Assert:Unexpected
+  cBugTranslation(
+    sOriginalBugTypeId = "Assert:AppExit",
+    asOriginalTopStackFrameSymbols = [
+      "*!wil::details::in1diag3::FailFast_Unexpected",
+    ],
+    sTranslatedBugTypeId = "Assert:Unexpected",
+    sTranslatedBugDescription = "The application triggered a fail fast application exit to indicate an assertion failed.",
+    sTranslatedSecurityImpact = "Unlikely to be exploitable, unless you can find a way to avoid this fail fast application exit.",
+  ),
+  # Assert:Breakpoint -> Assert:Unexpected
+  cBugTranslation(
+    sOriginalBugTypeId = "Assert:Breakpoint",
+    asOriginalTopStackFrameSymbols = [
+      "*!wil::details::in1diag3::FailFast_Unexpected",
+    ],
+    sTranslatedBugTypeId = "Assert:Unexpected",
+    sTranslatedBugDescription = "The application triggered a breakpoint exception to indicate an assertion failed.",
+    sTranslatedSecurityImpact = "Unlikely to be exploitable, unless you can find a way to avoid this breakpoint exception.",
   ),
 ];
