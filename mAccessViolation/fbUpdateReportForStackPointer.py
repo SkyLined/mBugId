3162f29674f57f232bb7acdced13074b190c586a
SkyLined <github@skylined.nl>
2018-02-14 22:46:37 +0100

    Update code for new stack address naming convention

--- mAccessViolation/fbUpdateReportForStackPointer.py
+++ mAccessViolation/fbUpdateReportForStackPointer.py
@@ -8,16 +8,16 @@ def fbUpdateReportForStackPointer(
 ):
   # See if the address is near the stack for the current thread:
   oThreadEnvironmentBlock = cThreadEnvironmentBlock.foCreateForCurrentThread(oProcess);
-  uOffsetFromTopOfStack = uAccessViolationAddress - oThreadEnvironmentBlock.uStackTopAddress;
-  uOffsetFromBottomOfStack = oThreadEnvironmentBlock.uStackBottomAddress - uAccessViolationAddress;
-  if uOffsetFromTopOfStack >= 0 and uOffsetFromTopOfStack <= oSystemInfo.uPageSize:
-    oBugReport.sBugTypeId = "AV%s[Stack]+%s" % (sViolationTypeId, fsGetNumberDescription(uOffsetFromTopOfStack));
-    oBugReport.sBugDescription = "Access violation while %s memory at 0x%X; %d/0x%X bytes passed the top of the stack at 0x%X." % \
-        (sViolationTypeDescription, uAccessViolationAddress, uOffsetFromTopOfStack, uOffsetFromTopOfStack, oThreadEnvironmentBlock.uStackTopAddress);
-  elif uOffsetFromBottomOfStack >= 0 and uOffsetFromBottomOfStack <= oSystemInfo.uPageSize:
-    oBugReport.sBugTypeId = "AV%s[Stack]-%s" % (sViolationTypeId, fsGetNumberDescription(uOffsetFromBottomOfStack));
-    oBugReport.sBugDescription = "Access violation while %s memory at 0x%X; %d/0x%X bytes before the bottom of the stack at 0x%X." % \
-        (sViolationTypeDescription, uAccessViolationAddress, uOffsetFromBottomOfStack, uOffsetFromBottomOfStack, oThreadEnvironmentBlock.uStackTopAddress);
+  uOffsetFromEndOfStack = uAccessViolationAddress - oThreadEnvironmentBlock.uStackAllocationEndAddress;
+  uOffsetFromStartOfStack = oThreadEnvironmentBlock.uStackAllocationStartAddress - uAccessViolationAddress;
+  if uOffsetFromEndOfStack >= 0 and uOffsetFromEndOfStack <= oSystemInfo.uPageSize:
+    oBugReport.sBugTypeId = "AV%s[Stack]+%s" % (sViolationTypeId, fsGetNumberDescription(uOffsetFromEndOfStack));
+    oBugReport.sBugDescription = "Access violation while %s memory at 0x%X; %d/0x%X bytes passed the end of the stack at 0x%X." % \
+        (sViolationTypeDescription, uAccessViolationAddress, uOffsetFromEndOfStack, uOffsetFromEndOfStack, oThreadEnvironmentBlock.uStackEndAddress);
+  elif uOffsetFromStartOfStack >= 0 and uOffsetFromStartOfStack <= oSystemInfo.uPageSize:
+    oBugReport.sBugTypeId = "AV%s[Stack]-%s" % (sViolationTypeId, fsGetNumberDescription(uOffsetFromStartOfStack));
+    oBugReport.sBugDescription = "Access violation while %s memory at 0x%X; %d/0x%X bytes before the start of the stack at 0x%X." % \
+        (sViolationTypeDescription, uAccessViolationAddress, uOffsetFromStartOfStack, uOffsetFromStartOfStack, oThreadEnvironmentBlock.uStackEndAddress);
   else:
     return False;
   oBugReport.sSecurityImpact = "Potentially exploitable security issue.";
