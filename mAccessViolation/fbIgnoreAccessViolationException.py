45e6f036b5939aea907273e2364d6ee142d4a3cd
SkyLined <github@skylined.nl>
2018-02-23 10:47:39 +0100

    Handle "cmp" instruction in collateral

--- mAccessViolation/fbIgnoreAccessViolationException.py
+++ mAccessViolation/fbIgnoreAccessViolationException.py
@@ -41,6 +41,7 @@ def fbIgnoreAccessViolationException(oCollateralBugHandler, oCdbWrapper, uProces
   uInstructionPointer = oProcess.fuGetValueForRegister("$ip", "Get current instruction pointer");
   oVirtualAllocation = oProcess.foGetVirtualAllocationForAddress(uInstructionPointer);
   if not oVirtualAllocation.bExecutable:
+    print "Memory at instruction pointer is not executable!?";
     return False; # This memory is not executable: we cannot continue execution.
   asUnassembleOutput = oProcess.fasExecuteCdbCommand(
     sCommand = "u 0x%X L2" % uInstructionPointer, 
@@ -48,6 +49,7 @@ def fbIgnoreAccessViolationException(oCollateralBugHandler, oCdbWrapper, uProces
   );
   if len(asUnassembleOutput) == 0:
     # The instruction pointer does not point to valid memory.
+    print "Cannot disassemble code at instruction pointer!?";
     return False;
   oCurrentInstructionMatch = len(asUnassembleOutput) == 3 and \
       re.match(r"^[0-9`a-f]+\s+[0-9`a-f]+\s+(\w+)(?:\s+(.*))?$", asUnassembleOutput[1], re.I);
@@ -60,95 +62,112 @@ def fbIgnoreAccessViolationException(oCollateralBugHandler, oCdbWrapper, uProces
   (sCurrentInstructionName, sCurrentInstructionArguments) = oCurrentInstructionMatch.groups();
   (sNextInstructionAddress,) = oNextInstructionMatch.groups();
   uNextInstructionAddress = long(sNextInstructionAddress.replace("`", ""), 16);
-  if sCurrentInstructionName in ["add", "addsd", "mov", "movzx", "movsd", "mulsd", "sub", "subsd"]:
-    # We do not care about the difference between these operations; for writes, we simply discard the value. For reads
-    # we assume that we have full control over the memory at the specified address, and thus full control over the
-    # result stored in the register, so we just fake reading a poisoned value.
-    asCurrentInstructionArguments = sCurrentInstructionArguments.split(",");
-    assert len(asCurrentInstructionArguments) == 2, \
-        "Instruction on line 2 appears to have %d arguments instead of 2:\r\n%s" % \
-        (len(asCurrentInstructionArguments), "\r\n".join(asUnassembleOutput));
-    sInstructionPointerRegister = {"x86": "eip", "x64": "rip"}[oProcess.sISA];
-    if sViolationTypeId == "R":
-      # We fake read AVs that read memory into a register by setting that register to a poisoned value and advancing
-      # the instruction pointer to the next instruction.
-      sDestinationRegister = asCurrentInstructionArguments[0];
-      uDestinationSizeInBits = gduSizeInBits_by_sRegisterName[sDestinationRegister];
-      assert uDestinationSizeInBits is not None, \
-          "Unrecognised `mov` instruction first/destination argument (expected register, got %s):\r\n%s" % \
-          (sDestinationRegister, "\r\n".join(asUnassembleOutput));
-      oSourceArgumentMatch = re.match("^(byte|(?:d|q|[xy]?(mm))?word) ptr \[.*\]$", asCurrentInstructionArguments[1]);
-      assert oSourceArgumentMatch, \
-          "Unrecognized `mov` instruction second/source argument (expected `... ptr [...]`, got %s):\r\n%s" % \
-          (asCurrentInstructionArguments[1], "\r\n".join(asUnassembleOutput));
-      sPointerType, bIsMMPointer = oSourceArgumentMatch.groups();
-      uSourceSizeInBits = gduSizeInBits_by_sPointerType[sPointerType];
-      if not bIsMMPointer:
-        uPoisonValue = oCollateralBugHandler.fuGetPoisonedValue(oProcess, uSourceSizeInBits, uPointerSizedValue);
-#        print "Faked read %d bits (0x%X) into %d bits %s" % (uSourceSize, uPoisonValue, uDestinationSizeInBits, sDestinationRegister);
-        asSpoofInstructionOutput = oProcess.fasExecuteCdbCommand(
-          sCommand = "r @%s=0x%X,@%s=0x%X,@%s,@%s;" % (
-            sDestinationRegister, uPoisonValue,
-            sInstructionPointerRegister, uNextInstructionAddress,
-            sDestinationRegister, sInstructionPointerRegister,
-          ),
-          sComment = "Fake and skip instruction that caused the read AV",
-        );
-      else:
-        # This is a bit more involved, as we can only set entire MM registers using 4 floating point numbers and we
-        # want to set it to one or more pointer sized integers, which may mean we only want to set part of the register.
-        if uSourceSizeInBits != uDestinationSizeInBits:
-          # We want to write only part of the register: read the current value so we can preserve part of it.
-          asGetDestinationBytesOutput = oProcess.fasExecuteCdbCommand(
-            sCommand = "r @%s:%dub;" % (sDestinationRegister, uDestinationSizeInBits / 8),
-            sComment = "Get current value of register",
-          );
-#          print "asGetDestinationBytesOutput: %s" % repr(asGetDestinationBytesOutput);
-          assert len(asGetDestinationBytesOutput) == 1 and asGetDestinationBytesOutput[0].startswith("%s=" % sDestinationRegister), \
-              "Unexpected register value output:\r\n%s" % "\r\n".join(asGetDestinationBytesOutput);
-          auDestinationRegisterBytes = [long(s, 16) for s in asGetDestinationBytesOutput[0].split("=", 1)[1].split(" ") if s];
-          auDestinationRegisterBytes.reverse();
-        else:
-          # Everything will be overwritten.
-          auDestinationRegisterBytes = [0 for x in xrange(uDestinationSizeInBits / 8)];
-#        print "auDestinationRegisterBytes: %s" % repr(auDestinationRegisterBytes);
-        # We will fake read as many pointer sized values from the collateral poison value queue as will fit in the
-        # source value.
-        uPointerSizeInBits = oProcess.uPointerSize * 8;
-        uSourceSizeInPointers = uSourceSizeInBits / uPointerSizeInBits;
-        uIndex = 0;
-        for x in xrange(uSourceSizeInPointers):
-          uPoisonValue = oCollateralBugHandler.fuGetPoisonedValue(oProcess, uPointerSizeInBits, uPointerSizedValue);
-          # The bytes in this poison value are written to the destination register buffer.
-          for y in xrange(uPointerSizeInBits / 8):
-            auDestinationRegisterBytes[uIndex] = uPoisonValue & 0xFF;
-            uPoisonValue >>= 8;
-            uIndex += 1;
-#        print "auDestinationRegisterBytes: %s" % repr(auDestinationRegisterBytes);
-        # We now know the bytes we want in the destination register, but we can only set it as 4 float values. So we
-        # will convert these bytes to four floats:
-        sDestinationRegisterBytes = "".join([chr(uByte) for uByte in auDestinationRegisterBytes]);
-        uFloatSizeInBits = uDestinationSizeInBits / 4;
-        anDestinationRegisterFloats = struct.unpack(">%s" % ({32: "f", 64: "d"}[uFloatSizeInBits] * 4), sDestinationRegisterBytes);
-#        print "anDestinationRegisterFloats: %s" % repr(anDestinationRegisterFloats);
-        sDestinationRegisterFloats = " ".join(["%f" % nFloat for nFloat in anDestinationRegisterFloats]);
-        # Overwrite the register to fake a read instruction and update the instruction pointer to the next instruction.
-        asSpoofInstructionOutput = oProcess.fasExecuteCdbCommand(
-          sCommand = "r @%s=%s,@%s=0x%X,@%s,@%s;" % (
-            sDestinationRegister, sDestinationRegisterFloats,
-            sInstructionPointerRegister, uNextInstructionAddress,
-            sDestinationRegister, sInstructionPointerRegister,
-          ),
-          sComment = "Fake and skip instruction that caused the read AV",
-        );
-#        print "asSpoofInstructionOutput: %s" % repr(asSpoofInstructionOutput);
-    else:
-      # We fake write AVs that write a register to memory by advancing the instruction pointer to the next
-      # instruction.
-      asSpoofInstructionOutput = oProcess.fasExecuteCdbCommand(
-        sCommand = "r @%s=0x%X;" % (sInstructionPointerRegister, uNextInstructionAddress),
-        sComment = "Skip instruction that caused the write AV",
-      );
-    return True;
-  else:
+  if sCurrentInstructionName not in ["add", "addsd", "cmp", "mov", "movzx", "movsd", "mulsd", "sub", "subsd"]:
+    print "Cannot handle instruction %s!?" % repr(sCurrentInstructionName);
     return False;
+  if sCurrentInstructionName in ["cmp"]:
+    # This instruction will affect flags, so we'll read 8 bits to use as flags from the poisoned values.
+    uPoisonFlagsValue = oCollateralBugHandler.fuGetPoisonedValue(oProcess, 8, uPointerSizedValue);
+    asFlags = ["of", "sf", "zf", "af", "pf", "cf"];
+    print "r %s;" % ",".join([
+        "@%s=%d" % (asFlags[uIndex], (uPoisonFlagsValue >> uIndex) & 1)
+        for uIndex in xrange(len(asFlags))
+      ]);
+    asSpoofInstructionFlagsOutput = oProcess.fasExecuteCdbCommand(
+      sCommand = "r %s;" % ",".join([
+        "@%s=%d" % (asFlags[uIndex], (uPoisonFlagsValue >> uIndex) & 1)
+        for uIndex in xrange(len(asFlags))
+      ]),
+      sComment = "Set flags for instruction that caused the AV",
+    );
+  # We do not care about the difference between these operations; for writes, we simply discard the value. For reads
+  # we assume that we have full control over the memory at the specified address, and thus full control over the
+  # result stored in the register, so we just fake reading a poisoned value.
+  asCurrentInstructionArguments = sCurrentInstructionArguments.split(",");
+  assert len(asCurrentInstructionArguments) == 2, \
+      "Instruction on line 2 appears to have %d arguments instead of 2:\r\n%s" % \
+      (len(asCurrentInstructionArguments), "\r\n".join(asUnassembleOutput));
+  sInstructionPointerRegister = {"x86": "eip", "x64": "rip"}[oProcess.sISA];
+  if sViolationTypeId == "W" or sCurrentInstructionName in ["cmp"]:
+    # We fake write AVs that write a register to memory by advancing the instruction pointer to the next
+    # instruction. We do the same for compare instructions.
+    # This does not alter flags like a normal instruction!!!!
+    asSpoofInstructionOutput = oProcess.fasExecuteCdbCommand(
+      sCommand = "r @%s=0x%X;" % (sInstructionPointerRegister, uNextInstructionAddress),
+      sComment = "Skip instruction that caused the AV",
+    );
+    return True;
+  # We fake read AVs that read memory into a register by setting that register to a poisoned value and advancing
+  # the instruction pointer to the next instruction.
+  # This does not alter flags like a normal instruction!!!!
+  sDestinationRegister = asCurrentInstructionArguments[0];
+  uDestinationSizeInBits = gduSizeInBits_by_sRegisterName[sDestinationRegister];
+  assert uDestinationSizeInBits is not None, \
+      "Unrecognised `mov` instruction first/destination argument (expected register, got %s):\r\n%s" % \
+      (sDestinationRegister, "\r\n".join(asUnassembleOutput));
+  oSourceArgumentMatch = re.match("^(byte|(?:d|q|[xy]?(mm))?word) ptr \[.*\]$", asCurrentInstructionArguments[1]);
+  assert oSourceArgumentMatch, \
+      "Unrecognized `mov` instruction second/source argument (expected `... ptr [...]`, got %s):\r\n%s" % \
+      (asCurrentInstructionArguments[1], "\r\n".join(asUnassembleOutput));
+  sPointerType, bIsMMPointer = oSourceArgumentMatch.groups();
+  uSourceSizeInBits = gduSizeInBits_by_sPointerType[sPointerType];
+  if not bIsMMPointer:
+    uPoisonValue = oCollateralBugHandler.fuGetPoisonedValue(oProcess, uSourceSizeInBits, uPointerSizedValue);
+#    print "Faked read %d bits (0x%X) into %d bits %s" % (uSourceSize, uPoisonValue, uDestinationSizeInBits, sDestinationRegister);
+    asSpoofInstructionOutput = oProcess.fasExecuteCdbCommand(
+      sCommand = "r @%s=0x%X,@%s=0x%X,@%s,@%s;" % (
+        sDestinationRegister, uPoisonValue,
+        sInstructionPointerRegister, uNextInstructionAddress,
+        sDestinationRegister, sInstructionPointerRegister,
+      ),
+      sComment = "Fake and skip instruction that caused the read AV",
+    );
+    return True;
+  # This is a bit more involved, as we can only set entire MM registers using 4 floating point numbers and we
+  # want to set it to one or more pointer sized integers, which may mean we only want to set part of the register.
+  if uSourceSizeInBits != uDestinationSizeInBits:
+    # We want to write only part of the register: read the current value so we can preserve part of it.
+    asGetDestinationBytesOutput = oProcess.fasExecuteCdbCommand(
+      sCommand = "r @%s:%dub;" % (sDestinationRegister, uDestinationSizeInBits / 8),
+      sComment = "Get current value of register",
+    );
+#    print "asGetDestinationBytesOutput: %s" % repr(asGetDestinationBytesOutput);
+    assert len(asGetDestinationBytesOutput) == 1 and asGetDestinationBytesOutput[0].startswith("%s=" % sDestinationRegister), \
+        "Unexpected register value output:\r\n%s" % "\r\n".join(asGetDestinationBytesOutput);
+    auDestinationRegisterBytes = [long(s, 16) for s in asGetDestinationBytesOutput[0].split("=", 1)[1].split(" ") if s];
+    auDestinationRegisterBytes.reverse();
+  else:
+    # Everything will be overwritten.
+    auDestinationRegisterBytes = [0 for x in xrange(uDestinationSizeInBits / 8)];
+#  print "auDestinationRegisterBytes: %s" % repr(auDestinationRegisterBytes);
+  # We will fake read as many pointer sized values from the collateral poison value queue as will fit in the
+  # source value.
+  uPointerSizeInBits = oProcess.uPointerSize * 8;
+  uSourceSizeInPointers = uSourceSizeInBits / uPointerSizeInBits;
+  uIndex = 0;
+  for x in xrange(uSourceSizeInPointers):
+    uPoisonValue = oCollateralBugHandler.fuGetPoisonedValue(oProcess, uPointerSizeInBits, uPointerSizedValue);
+    # The bytes in this poison value are written to the destination register buffer.
+    for y in xrange(uPointerSizeInBits / 8):
+      auDestinationRegisterBytes[uIndex] = uPoisonValue & 0xFF;
+      uPoisonValue >>= 8;
+      uIndex += 1;
+#  print "auDestinationRegisterBytes: %s" % repr(auDestinationRegisterBytes);
+  # We now know the bytes we want in the destination register, but we can only set it as 4 float values. So we
+  # will convert these bytes to four floats:
+  sDestinationRegisterBytes = "".join([chr(uByte) for uByte in auDestinationRegisterBytes]);
+  uFloatSizeInBits = uDestinationSizeInBits / 4;
+  anDestinationRegisterFloats = struct.unpack(">%s" % ({32: "f", 64: "d"}[uFloatSizeInBits] * 4), sDestinationRegisterBytes);
+#  print "anDestinationRegisterFloats: %s" % repr(anDestinationRegisterFloats);
+  sDestinationRegisterFloats = " ".join(["%f" % nFloat for nFloat in anDestinationRegisterFloats]);
+  # Overwrite the register to fake a read instruction and update the instruction pointer to the next instruction.
+  asSpoofInstructionOutput = oProcess.fasExecuteCdbCommand(
+    sCommand = "r @%s=%s,@%s=0x%X,@%s,@%s;" % (
+      sDestinationRegister, sDestinationRegisterFloats,
+      sInstructionPointerRegister, uNextInstructionAddress,
+      sDestinationRegister, sInstructionPointerRegister,
+    ),
+    sComment = "Fake and skip instruction that caused the read AV",
+  );
+#  print "asSpoofInstructionOutput: %s" % repr(asSpoofInstructionOutput);
+  return True;
