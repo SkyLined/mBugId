e89957f802b8b66301ea65c90648777c74716ac8
SkyLined <github@skylined.nl>
2018-03-08 10:06:37 +0100

    Faster code

--- cCdbWrapper_fApplicationStdOutOrErrThread.py
+++ cCdbWrapper_fApplicationStdOutOrErrThread.py
@@ -4,23 +4,20 @@ gbDebugIO = False; # Used for debugging cdb I/O issues
 
 def cCdbWrapper_fApplicationStdOutOrErrThread(oCdbWrapper, oConsoleProcess, oStdOutOrErrPipe):
   uProcessId = oConsoleProcess.uId;
+  sEventName = {
+    "StdOut": "Application stdout output",
+    "StdErr": "Application stderr output",
+  }[oStdOutOrErrPipe.sDescription];
   while 1:
     try:
       sLine = oStdOutOrErrPipe.fsReadLine();
     except IOError:
       break;
     if gbDebugIO: print "\r<app:%d/0x%X:%s<%s" % (oConsoleProcess.uId, oConsoleProcess.uId, oStdOutOrErrPipe.sDescription, sLine);
-    sEventName = {
-      "StdOut": "Application stdout output",
-      "StdErr": "Application stderr output",
-    }[oStdOutOrErrPipe.sDescription];
     
     if oCdbWrapper.bGenerateReportHTML:
       oCdbWrapper.sCdbIOHTML += "<span class=\"Application%s\">%s</span><br/>\n" % \
           (oStdOutOrErrPipe.sDescription, oCdbWrapper.fsHTMLEncode(sLine, uTabStop = 8));
-    oCdbWrapper.fbFireEvent(
-      sEventName,
-      oConsoleProcess,
-      sLine
-    );
   if gbDebugIO: print "\r<app:%d/0x%X:%s:EOF" % (oConsoleProcess.uId, oConsoleProcess.uId, oStdOutOrErrPipe.sDescription);
+    oCdbWrapper.fbFireEvent(sEventName, oConsoleProcess, sLine);
+  
