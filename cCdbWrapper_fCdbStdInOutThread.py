633c1ab3bb0bec2aafd58b345533034c463124c9
SkyLined <github@skylined.nl>
2018-05-17 15:15:57 +0200

    fix cdb ISA selection command code

--- cCdbWrapper_fCdbStdInOutThread.py
+++ cCdbWrapper_fCdbStdInOutThread.py
@@ -246,7 +246,11 @@ def cCdbWrapper_fCdbStdInOutThread(oCdbWrapper):
       oCdbWrapper.oCdbCurrentThread = oCdbWrapper.oCdbCurrentProcess.foGetThreadForId(uThreadId);
       if oCdbWrapper.oCdbCurrentProcess.sISA != oCdbWrapper.sCdbCurrentISA:
         # Select process ISA if it is not yet the current ISA. ".block{}" is required
-        sSelectCommand += ".block{.effmach %s;};" % {"x86": "x86", "x64": "amd64"}[oCdbWrapper.oCdbCurrentProcess.sISA];
+        oCdbWrapper.fasExecuteCdbCommand(
+          sCommand = ".effmach %s;" % {"x86": "x86", "x64": "amd64"}[oCdbWrapper.oCdbCurrentProcess.sISA],
+          sComment = "Switch to current process ISA",
+          bRetryOnTruncatedOutput = True,
+        );
         # Assuming there's no error, track the new current isa.
         oCdbWrapper.sCdbCurrentISA = oCdbWrapper.oCdbCurrentProcess.sISA;
       asDebugOutput = [
@@ -334,7 +338,11 @@ def cCdbWrapper_fCdbStdInOutThread(oCdbWrapper):
     oCdbWrapper.oCdbCurrentThread = oCdbWrapper.oCdbCurrentProcess.foGetThreadForId(uThreadId);
     if oCdbWrapper.oCdbCurrentProcess.sISA != oCdbWrapper.sCdbCurrentISA:
       # Select process ISA if it is not yet the current ISA. ".block{}" is required
-      sSelectCommand += ".block{.effmach %s;};" % {"x86": "x86", "x64": "amd64"}[oCdbWrapper.oCdbCurrentProcess.sISA];
+      oCdbWrapper.fasExecuteCdbCommand(
+        sCommand = ".effmach %s;" % {"x86": "x86", "x64": "amd64"}[oCdbWrapper.oCdbCurrentProcess.sISA],
+        sComment = "Switch to current process ISA",
+        bRetryOnTruncatedOutput = True,
+      );
       # Assuming there's no error, track the new current isa.
       oCdbWrapper.sCdbCurrentISA = oCdbWrapper.oCdbCurrentProcess.sISA;
     ### Free reserve memory for exception analysis ###################################################################
