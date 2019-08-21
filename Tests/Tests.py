6c2c01b98bd24a6ea47a81964833381a56c8d61f
SkyLined <github@skylined.nl>
2019-08-21 12:11:46 +0200

    Upgrade code to check imports

--- Tests/Tests.py
+++ Tests/Tests.py
@@ -1,50 +1,67 @@
-import os, platform, re, sys, threading, time, traceback;
+import json, os, sys;
 
-# Augment the search path to make cBugId a package and have access to its modules folder.
+# Augment the search path to make the test subject a package and have access to its modules folder.
 sTestsFolderPath = os.path.dirname(os.path.abspath(__file__));
 sMainFolderPath = os.path.dirname(sTestsFolderPath);
 sParentFolderPath = os.path.dirname(sMainFolderPath);
 sModulesFolderPath = os.path.join(sMainFolderPath, "modules");
 asOriginalSysPath = sys.path[:];
 sys.path = [sParentFolderPath, sModulesFolderPath] + asOriginalSysPath;
+# Load product details
+oProductDetailsFile = open(os.path.join(sMainFolderPath, "dxProductDetails.json"), "rb");
+try:
+  dxProductDetails = json.load(oProductDetailsFile);
+finally:
+  oProductDetailsFile.close();
 # Save the list of names of loaded modules:
 asOriginalModuleNames = sys.modules.keys();
 
-import cBugId, mFileSystem2, mWindowsAPI;
-from mDebugOutput import fShowFileDebugOutputForClass;
-from mMultiThreading import cLock, cThread, cWithCallbacks;
-
-# Restore the search path
-sys.path = asOriginalSysPath;
-
-#fShowFileDebugOutputForClass(cLock);
-#fShowFileDebugOutputForClass(cThread);
-#fShowFileDebugOutputForClass(cWithCallbacks);
+__import__(dxProductDetails["sProductName"], globals(), locals(), [], -1);
 
 # Sub-packages should load all modules relative, or they will end up in the global namespace, which means they may get
 # loaded by the script importing it if it tries to load a differnt module with the same name. Obviously, that script
 # will probably not function when the wrong module is loaded, so we need to check that we did this correctly.
-for sModuleName in sys.modules.keys():
-  assert (
-    sModuleName in asOriginalModuleNames # This was loaded before cBugId was loaded
-    or sModuleName.lstrip("_").split(".", 1)[0] in [
-      "cBugId", # This was loaded as part of the cBugId package
-      # These packages are loaded by cBugId:
-      "mDateTime", "mDebugOutput", "mFileSystem2", "mMultiThreading", "mProductDetails", "mWindowsAPI", "mWindowsSDK",
-      "oConsole",
-      # These built-in modules are loaded by these packages:
-      "base64", "binascii", "bz2", "calendar", "contextlib", "cStringIO", "ctypes", "datetime", "encodings",
-      "fnmatch", "gc", "hashlib", "hmac", "io", "json", "math", "msvcrt", "nturl2path", "shutil", "socket", "ssl",
-      "struct", "subprocess", "textwrap", "Queue", "urllib", "urlparse", "uuid", "winreg", "zipfile", "zlib",
-    ]
-  ), \
-      "Module %s was unexpectedly loaded outside of the cBugId package!" % sModuleName;
+asUnexpectedModules = list(set([
+  sModuleName.lstrip("_").split(".", 1)[0] for sModuleName in sys.modules.keys()
+  if not (
+    sModuleName in asOriginalModuleNames # This was loaded before
+    or sModuleName.lstrip("_").split(".", 1)[0] in (
+      [dxProductDetails["sProductName"]] +
+      dxProductDetails["asDependentOnProductNames"] +
+      [
+        # This is optional, not required:
+        "oConsole",
+        # These built-in modules are expected:
+        "base64", "binascii", "bz2", "calendar", "collections", "contextlib",
+        "cStringIO", "ctypes", "datetime", "encodings", "fnmatch", "gc",
+        "hashlib", "heapq", "hmac", "io", "itertools", "json", "keyword",
+        "math", "msvcrt", "nturl2path", "platform", "shutil", "socket", "ssl",
+        "string", "strop", "struct", "subprocess", "textwrap", "thread",
+        "threading", "time", "Queue", "urllib", "urlparse", "uuid", "winreg",
+        "zipfile", "zlib",
+      ]
+    )
+  )
+]));
+assert len(asUnexpectedModules) == 0, \
+      "Module(s) %s was/were unexpectedly loaded!" % ", ".join(sorted(asUnexpectedModules));
 
+#Import the test subject
+import os, platform, re, threading, time, traceback;
 from cBugId import cBugId;
 from cBugId.mAccessViolation.fbUpdateReportForSpecialPointer import gddtsDetails_uSpecialAddress_sISA;
 import mFileSystem2;
 from mWindowsAPI import oSystemInfo, fsGetPythonISA;
 from mWindowsSDK import *;
+
+# Restore the search path
+sys.path = asOriginalSysPath;
+
+#from mDebugOutput import fShowFileDebugOutputForClass;
+#from mMultiThreading import cLock, cThread, cWithCallbacks;
+#fShowFileDebugOutputForClass(cLock);
+#fShowFileDebugOutputForClass(cThread);
+#fShowFileDebugOutputForClass(cWithCallbacks);
 
 gbDebugStartFinish = False;  # Show some output when a test starts and finishes.
 gbShowCdbIO = False;          # Show cdb I/O during tests (you'll want to run only 1 test at a time for this).
