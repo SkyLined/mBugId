85b9d1a77e80278e29888b0283d75d99368da262
SkyLined <github@skylined.nl>
2019-12-20 14:33:35 +0100

    Code cleanup

--- mAccessViolation/fUpdateReportForProcessThreadTypeIdAndAddress.py
+++ mAccessViolation/fUpdateReportForProcessThreadTypeIdAndAddress.py
@@ -32,6 +32,5 @@ def fUpdateReportForProcessThreadTypeIdAndAddress(oCdbWrapper, oBugReport, oProc
     ):
       # We handled the pointer as this type, and it can be only one type, so stop trying other types:
       return;
-  else:
-    # This pointer is not of a known type: should never happend
-    raise AssertionError("Could not handle AV%s@0x%08X" % (sViolationTypeId, uAccessViolationAddress));
+  # This pointer is not of a known type: should never happend
+  raise AssertionError("Could not handle AV%s@0x%08X" % (sViolationTypeId, uAccessViolationAddress));
