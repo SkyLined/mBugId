a871d6445f554d8e1702f517836be97f9e4bfdf1
SkyLined <github@skylined.nl>
2021-08-06 21:42:18 +0200

    Use `cProcess.fsb0GetSymbolForAddress` instead of inline code.

--- ft0oCallModuleAndFunctionFromCallInstructionForReturnAddress.py
+++ ft0oCallModuleAndFunctionFromCallInstructionForReturnAddress.py
@@ -6,33 +6,6 @@ cCallRel32InstructionStructure = iStructureType8.fcCreateClass("CALL_rel32",
   (INT32,       "i32Displacement"),
 );
 
-
-grbNoSymbol = re.compile(
-  rb"\A"                             # {
-  rb"(?:"                            #   either {
-    rb"[^!\+]+"                      #     sbModuleCdbId
-    rb"\+0x" rb"[0-9`a-f]+"          #     "+0x" sOffset
-    rb" \(" rb"[0-9`a-f]+" rb"\)"    #     " (" sAddress ")"
-  rb"|"                              #   } or {
-    rb"[0-9`a-f]+"                   #     sbAddress
-  rb")" rb"\s*"                      #   } [whitespace]
-  rb"\Z",                            # }
-  re.I
-);
-grbSymbol = re.compile(
-  rb"\A"                             # {
-  rb"([^!]+)"                        #   (sbModuleCdbId)
-  rb"!"                              #   "!"
-  rb"(.+?)"                          #   (sFunctionSymbolName)
-  rb"(?:"                            #   optional {
-    rb"\+0x" rb"[0-9`a-f]+"          #     "+0x" sOffset
-  rb")?"                             #   }
-  rb" \(" rb"[0-9`a-f]+" rb"\)"      #   " (" sbAddress ")"
-  rb"\s*"                            #   [whitespace]
-  rb"\Z",                            # }
-  re.I
-);
-
 def ft0oCallModuleAndFunctionFromCallInstructionForReturnAddress(oProcess, uReturnAddress):
   # The symbol for a chunk of code may not be correct when using export symbols, or if we're in a small branch that is
   # not marked correctly in the pdb (cdb will report the closest symbol, which may be for another function!).
@@ -58,30 +31,22 @@ def ft0oCallModuleAndFunctionFromCallInstructionForReturnAddress(oProcess, uRetu
     return None; # This opcode is not for the CALL instruction we can parse.
   # Find out the target of the call instruction (which is relative to the return address).
   uCallTargetAddress = uReturnAddress + oCallInstruction.i32Displacement;
-  # Ask cdb for the symbol at the call target address. This is done twice: once to make sure symbols are loaded (which
-  # may cause all kinds of random output that make the output unparsable) and once again to get parsable output.
-  oProcess.fasbExecuteCdbCommand(
-    sbCommand = b'.printf "%%y\\n", 0x%X;' % uCallTargetAddress, 
-    sb0Comment = b"Get symbol for call target (warmup to make sure symbols are loaded)",
-  );
-  asbSymbolOutput = oProcess.fasbExecuteCdbCommand(
-    sbCommand = b'.printf "%%y\\n", 0x%X;' % uCallTargetAddress, 
-    sb0Comment = b"Get symbol for call target",
-  );
-  # Output for an invalid (NULL) pointer:
-  #   >00000000
-  # Output for a module without symbol information (in x64 debugger):
-  #   >nmozglue+0xf0c4 (73f1f0c4)
-  # Output for a valid symbol (in x86 debugger, notice different header aligning):
-  #   >ntdll!DbgBreakPoint (77ec1250)
-  assert len(asbSymbolOutput) == 1, \
-      "Unexpected get symbol output:\r\n%s" % "\r\n".join(asbSymbolOutput);
-  if grbNoSymbol.match(asbSymbolOutput[0]):
-    return None; # There is no symbol for the call target address.
-  obSymbolOutput = grbSymbol.match(asbSymbolOutput[0]);
-  assert obSymbolOutput, \
-      "Unexpected symbol result:\r\n%s" % "\r\n".join(asbSymbolOutput);
-  sbModuleCdbId, sbFunctionSymbol = obSymbolOutput.groups();
-  oModule = oProcess.foGetOrCreateModuleForCdbId(sbModuleCdbId);
-  oFunction = oModule.foGetOrCreateFunctionForSymbol(sbFunctionSymbol);
-  return (oModule, oFunction);
+  sb0Symbol = oProcess.fsb0GetSymbolForAddress(uCallTargetAddress);
+  if sb0Symbol is None:
+    return None;
+  (
+    u0Address,
+    sb0UnloadedModuleFileName, o0Module, u0ModuleOffset,
+    o0Function, i0OffsetFromStartOfFunction
+  ) = oProcess.ftxSplitSymbolOrAddress(sb0Symbol);
+  # We only accept "<module>!<function>" symbols:
+  if (
+    u0Address is not None
+    or sb0UnloadedModuleFileName is not None
+    or o0Module is None
+    or u0ModuleOffset is not None
+    or o0Function is None
+    or i0OffsetFromStartOfFunction is not None
+  ):
+    return None;
+  return (o0Module, o0Function);
