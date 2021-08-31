837eb6a9e268b7a181ca058b0c1fcc9b9dcceb8e
SkyLined <github@skylined.nl>
2021-08-31 23:43:28 +0200

    Complete code changes (code was not finished before)

--- cProcess_fsb0GetSymbolForAddress.py
+++ cProcess_fsb0GetSymbolForAddress.py
@@ -1,10 +1,18 @@
+import re;
+
 from .fu0ValueFromCdbHexOutput import fu0ValueFromCdbHexOutput;
 
-def fs0ProcessOutputOrThrowExceptionIfItContainsJunk(uAddress, asbSymbolOutput):
+grbAddress = re.compile(
+  rb"^"
+  rb"[0-9`a-f]+"              # address
+  rb"$"
+);
+
+def fs0ProcessOutputOrThrowExceptionIfItContainsJunk(oProcess, uAddress, asbSymbolOutput):
   assert len(asbSymbolOutput) == 1, "";
   # If there is no symbol at the addres, the address will be output, which we
   # can detect, so we can return None:
-  if fu0ValueFromCdbHexOutput(asbSymbolOutput[0]) == uAddress:
+  if grbAddress.match(asbSymbolOutput[0]) and fu0ValueFromCdbHexOutput(asbSymbolOutput[0]) == uAddress:
     return None;
   # This will throw an exception is the output contains junk.
   txSplitSymbolOrAddress = oProcess.ftxSplitSymbolOrAddress(asbSymbolOutput[0]);
@@ -22,28 +30,30 @@ def cProcess_fsb0GetSymbolForAddress(oProcess, uAddress):
   #   >nmozglue+0xf0c4 (73f1f0c4)
   # Output for a valid symbol (in x86 debugger, notice different header aligning):
   #   >ntdll!DbgBreakPoint (77ec1250)
+  sbGetSymbolCommand = b'.printf "%%y\\n", 0x%X;' % uAddress;
   asbSymbolOutput = oProcess.fasbExecuteCdbCommand(
-    sbCommand = b'.printf "%%y\\n", 0x%X;' % uAddress, 
+    sbCommand = sbGetSymbolCommand, 
     sb0Comment = b"Get symbol for address (first attempt)",
   );
   try:
     # Check if the output is a valid symbol or address and return the symbol
     # or None respectively. Throw exception if not which we catch to try again.
-    return fs0ProcessOutputOrThrowExceptionIfItContainsJunk(uAddress, asbSymbolOutput);
+    return fs0ProcessOutputOrThrowExceptionIfItContainsJunk(oProcess, uAddress, asbSymbolOutput);
   except:
     pass;
   asbSymbolOutput = oProcess.fasbExecuteCdbCommand(
-    sbCommand = sbCommand, 
+    sbCommand = sbGetSymbolCommand, 
     sb0Comment = b"Get symbol for address (second attempt: cdb may have output symbol loading junk the first time)",
   );
   try:
     # Check if the output is a valid symbol or address again and return the
     # symbol or None respectively. Throw exception if not which we catch to
     # throw a more informative error:
-    return fs0ProcessOutputOrThrowExceptionIfItContainsJunk(uAddress, asbSymbolOutput);
+    return fs0ProcessOutputOrThrowExceptionIfItContainsJunk(oProcess, uAddress, asbSymbolOutput);
   except Exception as oException:
     raise AssertionError(
-      "Unexpected get symbol output (%s):\r\n%s" % (
+      "Unexpected get symbol output for %s (%s):\r\n%s" % (
+        repr(sbGetSymbolCommand),
         " | ".join(oException.args),
         "\r\n".join(str(sbLine, "ascii", "strict") for sbLine in asbSymbolOutput)
       )
