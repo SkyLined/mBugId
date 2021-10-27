3e7ee0fcdad2fd3810504abb3eb0a3ee8bdd96ab
SkyLined <github@skylined.nl>
2021-10-27 13:35:46 +0200

    Add `cEndOfCommandOutputMarkerMissingException.__str__`

--- cEndOfCommandOutputMarkerMissingException.py
+++ cEndOfCommandOutputMarkerMissingException.py
@@ -1,3 +1,5 @@
 class cEndOfCommandOutputMarkerMissingException(Exception):
-  def __init__(oEndOfCommandOutputMarkerMissingException, asCommandOutput):
-    oEndOfCommandOutputMarkerMissingException.asCommandOutput = asCommandOutput;
+  def __init__(oSelf, asbCommandOutput):
+    oSelf.asbCommandOutput = asbCommandOutput;
+  def __str__(oSelf):
+    return "The 'End-of-Command-Output Marker' is missing from the command output:\r\n%s" % "\r\n".join(oSelf.asbCommandOutput);
