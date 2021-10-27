b9e6a906b0b2b0772f216b2a3d5aded88ca543ad
SkyLined <github@skylined.nl>
2021-10-27 13:35:13 +0200

    Add `cProcess.fo0GetFunctionForAddress`

---
+++ cProcess_fo0GetFunctionForAddress.py
@@ -0,0 +1,10 @@
+﻿def cProcess_fo0GetFunctionForAddress(oProcess, uAddress):
+  sb0Symbol = oProcess.fsb0GetSymbolForAddress(uAddress, b"Get function symbol for address 0x%X" % uAddress);
+  if sb0Symbol is None:
+    return None;
+  (
+    u0Address,
+    sb0UnloadedModuleFileName, o0Module, u0ModuleOffset,
+    o0Function, i0OffsetFromStartOfFunction
+  ) = oProcess.ftxSplitSymbolOrAddress(sb0Symbol);
+  return o0Function;
