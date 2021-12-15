ef2d702d12f0ffd06b2469bd3de21fe3dca804de
SkyLined <github@skylined.nl>
2021-12-15 17:36:11 +0100

    Report internal errors in threads.

--- cCdbWrapper_fCdbStdErrHelperThread.py
+++ cCdbWrapper_fCdbStdErrHelperThread.py
@@ -1,22 +1,27 @@
 from .dxConfig import dxConfig;
 
 gbDebugIO = False; # Used for debugging cdb I/O issues
+from .cBugReport_UnhandledPythonException import cBugReport_UnhandledPythonException;
 
 def cCdbWrapper_fCdbStdErrHelperThread(oCdbWrapper):
-  sLine = "";
-  while 1:
-    s0Line = oCdbWrapper.oCdbConsoleProcess.oStdErrPipe.fs0ReadLine();
-    if s0Line is None:
-      if gbDebugIO: print("\r<stderr:EOF<");
-      oCdbWrapper.fbFireCallbacks("Log message", "Failed to read from cdb.exe stderr");
-      break;
-    if gbDebugIO: print("\r<stderr<%s" % s0Line);
-    if oCdbWrapper.bGenerateReportHTML:
-      sLineHTML = "<span class=\"CDBStdErr\">%s</span><br/>\n" % oCdbWrapper.fsHTMLEncode(s0Line, uTabStop = 8);
-      oCdbWrapper.sCdbIOHTML += sLineHTML;
-    oCdbWrapper.fbFireCallbacks("Log message", "StdErr output", {
-      "Line": s0Line,
-    });
-    oCdbWrapper.fbFireCallbacks("Cdb stderr output", s0Line);
-  oCdbWrapper.bCdbIsRunning = False;
+  try:
+    sLine = "";
+    while 1:
+      s0Line = oCdbWrapper.oCdbConsoleProcess.oStdErrPipe.fs0ReadLine();
+      if s0Line is None:
+        if gbDebugIO: print("\r<stderr:EOF<");
+        oCdbWrapper.fbFireCallbacks("Log message", "Failed to read from cdb.exe stderr");
+        break;
+      if gbDebugIO: print("\r<stderr<%s" % s0Line);
+      if oCdbWrapper.bGenerateReportHTML:
+        sLineHTML = "<span class=\"CDBStdErr\">%s</span><br/>\n" % oCdbWrapper.fsHTMLEncode(s0Line, uTabStop = 8);
+        oCdbWrapper.sCdbIOHTML += sLineHTML;
+      oCdbWrapper.fbFireCallbacks("Log message", "StdErr output", {
+        "Line": s0Line,
+      });
+      oCdbWrapper.fbFireCallbacks("Cdb stderr output", s0Line);
+    oCdbWrapper.bCdbIsRunning = False;
+  except Exception as oException:
+    oBugReport = cBugReport_UnhandledPythonException(oCdbWrapper, oException);
+    oBugReport.fReport();
 
