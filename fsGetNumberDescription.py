e34049d4ae2028042e9696ed76b43a87b4471502
SkyLined <cojmpjum_github.com@sxd.be>
2022-08-24 16:16:47 +0200

    Inline description

--- fsGetNumberDescription.py
+++ fsGetNumberDescription.py
@@ -1,6 +1,8 @@
 from .dxConfig import dxConfig;
 
 def fsGetNumberDescription(uNumber, sSign = "+"):
+  # This function converts a number into an architecture independent number.
+  # e.g. 1 -> 1, 7 -> 4n+3, see dxConfig for more details.
   uArchitectureIndependentBugIdBytes = (dxConfig["uArchitectureIndependentBugIdBits"] or 0) / 8;
   if uArchitectureIndependentBugIdBytes == 0 or uNumber < uArchitectureIndependentBugIdBytes:
     # Architecture independent bug ids are disabled, or the number is too small to require fixing.
