0d1acfaa5d7b0feb28258d80fc07510b9a7508dd
SkyLined <cojmpjum_github.com@sxd.be>
2023-06-01 14:42:47 +0200

    Improve bug translations

--- mBugTranslations/wil.py
+++ mBugTranslations/wil.py
@@ -15,62 +15,46 @@ aoBugTranslations = [
       rb".*!wil::details::WilRaiseFailFastException",
     ],
   ),
-  # Breakpoint -> Assert
-  cBugTranslation(
-   srzOriginalBugTypeId = "Breakpoint",
-    azs0rbAppliesOnlyToTopStackFrame = [
-      rb".*!wil::details::ReportFailure",
-    ],
-    s0zTranslatedBugTypeId = "Assert",
-    s0zTranslatedBugDescription = "The application triggered a debugger breakpoint to indicate an assertion failed.",
-    s0zTranslatedSecurityImpact = "Unlikely to be exploitable, unless you can find a way to avoid this breakpoint.",
-  ),
   # AppExit -> Assert
   cBugTranslation(
-   srzOriginalBugTypeId = "AppExit",
     azs0rbAppliesOnlyToTopStackFrame = [
       rb".*!wil::details::ReportFailure",
     ],
     s0zTranslatedBugTypeId = "Assert",
     s0zTranslatedBugDescription = "The application triggered a fail fast application exit to indicate an assertion failed.",
-    s0zTranslatedSecurityImpact = "Unlikely to be exploitable, unless you can find a way to avoid this fail fast application exit.",
+    s0zTranslatedSecurityImpact = "Unlikely to be exploitable.",
   ),
   # Assert -> Assert:Win32
   cBugTranslation(
-   srzOriginalBugTypeId = "Assert",
     azs0rbAppliesOnlyToTopStackFrame = [
-      rb".*!wil::details::(?:ReportFailure_Win32|in1diag3::_?FailFast_Win32)",
+      rb".*!wil::details::(?:ReportFailure|in1diag3::_?FailFast)_Win32",
     ],
     s0zTranslatedBugTypeId = "Assert:Win32",
   ),
   # Assert -> Assert:HRESULT
   cBugTranslation(
-   srzOriginalBugTypeId = "Assert",
     azs0rbAppliesOnlyToTopStackFrame = [
-      rb".*!wil::details::(?:ReportFailure_Hr|in1diag3::_?FailFast_Hr)",
+      rb".*!wil::details::(?:ReportFailure|in1diag3::_?FailFast)_Hr",
     ],
     s0zTranslatedBugTypeId = "Assert:HRESULT",
   ),
   # Assert -> Assert:Unexpected
   cBugTranslation(
-   srzOriginalBugTypeId = "Assert",
     azs0rbAppliesOnlyToTopStackFrame = [
-      rb".*!wil::details::(?:ReportFailure_Unexpected|in1diag3::_?FailFast_Unexpected)",
+      rb".*!wil::details::(?:ReportFailure|in1diag3::_?FailFast)_Unexpected",
     ],
     s0zTranslatedBugTypeId = "Assert:Unexpected",
   ),
   # Assert -> OOM
   cBugTranslation(
-   srzOriginalBugTypeId = "Assert",
     azs0rbAppliesOnlyToTopStackFrame = [
-      rb".*!wil::details::in1diag3::_?FailFast_NullAlloc",
+      rb".*!wil::details::(?:ReportFailure|in1diag3::_?FailFast)_NullAlloc",
     ],
     s0zTranslatedBugTypeId = "OOM",
     s0zTranslatedBugDescription = "The application was unable to allocate enough memory.",
   ),
   # Assert:HRESULT -> OOM
   cBugTranslation(
-   srzOriginalBugTypeId = "Assert:HRESULT",
     azs0rbAppliesOnlyToTopStackFrame = [
       rb".*!wil::details::in1diag3::_?FailFast_NullAlloc",
     ],
