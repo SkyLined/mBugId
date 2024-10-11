51033c16cd33d5c5a3b440a7d930b833f5690f82
SkyLined <cojmpjum_github.com@sxd.be>
2024-10-11 10:19:17 +0200

    Ignore more functions in kernelbase.dll

--- mBugTranslations/kernelbase_dll.py
+++ mBugTranslations/kernelbase_dll.py
@@ -5,11 +5,12 @@ aoBugTranslations = [
   cBugTranslation(
     azs0rbAdditionalIrrelevantStackFrameSymbols = [
       rb"kernelbase\.dll!DebugBreak",
+      rb"kernelbase\.dll!FindClose",
+      rb"kernelbase\.dll!LocalFree",
       rb"kernelbase\.dll!RaiseException",
       rb"kernelbase\.dll!RaiseFailFastException",
       rb"kernelbase\.dll!UnhandledExceptionFilter",
       rb"kernelbase\.dll!TerminateProcessOnMemoryExhaustion",
-      rb"kernelbase\.dll!LocalFree",
     ],
   ),
 ];
