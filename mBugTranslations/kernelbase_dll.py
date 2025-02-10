8029cb95b41949cd69bbaa7a465d3266d073709b
SkyLined <1959110+SkyLined@users.noreply.github.com>
2025-02-10 16:00:14 +0100

    Update kernelbase_dll.py

--- mBugTranslations/kernelbase_dll.py
+++ mBugTranslations/kernelbase_dll.py
@@ -1,14 +1,14 @@
 from .cBugTranslation import cBugTranslation;
 
 aoBugTranslations = [
-  # * -> hide irrelevant frames
   cBugTranslation(
     azs0rbAdditionalIrrelevantStackFrameSymbols = [
+      # There are a lot of helper functions that clutter the stack without
+      # providing insight into the issue. These are all hidden by BugId:
       rb"kernelbase\.dll!DebugBreak",
       rb"kernelbase\.dll!FindClose",
       rb"kernelbase\.dll!LocalFree",
-      rb"kernelbase\.dll!RaiseException",
-      rb"kernelbase\.dll!RaiseFailFastException",
+      rb"kernelbase\.dll!Raise.*Exception",
       rb"kernelbase\.dll!UnhandledExceptionFilter",
       rb"kernelbase\.dll!TerminateProcessOnMemoryExhaustion",
     ],
