e719f09094130f97da173bac812beda6f119fcca
SkyLined <1959110+SkyLined@users.noreply.github.com>
2025-02-10 16:00:07 +0100

    Update ntdll_dll.py

--- mBugTranslations/ntdll_dll.py
+++ mBugTranslations/ntdll_dll.py
@@ -1,30 +1,27 @@
 from .cBugTranslation import cBugTranslation;
 
 aoBugTranslations = [
-  # These frames are never relevant
   cBugTranslation(
     azs0rbAdditionalIrrelevantStackFrameSymbols = [
+      # There are a lot of helper functions that clutter the stack without
+      # providing insight into the issue. These are all hidden by BugId:
       rb"ntdll\.dll!_C_specific_handler",
       rb"ntdll\.dll!DbgBreakPoint",
-      rb"ntdll\.dll!DbgPrintEx",
       rb"ntdll\.dll!DbgUiRemoteBreakin",
       rb"ntdll\.dll!FindNodeOrParent",
-      rb"ntdll\.dll!KiRaiseUserExceptionDispatcher",
-      rb"ntdll\.dll!KiUserExceptionDispatch",
-      rb"ntdll\.dll!LdrpHandleInvalidUserCallTarget",
-      rb"ntdll\.dll!RtlDeleteCriticalSection",
+      rb"ntdll\.dll!Ki.*UserExceptionDispatcher",
+      rb"ntdll\.dll!.*HandleInvalidUserCallTarget",
       rb"ntdll\.dll!RtlDispatchException",
-      rb"ntdll\.dll!RtlEnterCriticalSection",
+      rb"ntdll\.dll!Rtl.*CriticalSection",
       rb"ntdll\.dll!RtlFailFast\d*",
       rb"ntdll\.dll!RtlInsertElementGenericTableAvl",
-      rb"ntdll\.dll!Rtlp?(Debug)?(Allocate|Free)Heap\w*",
+      rb"ntdll\.dll!Rtl.*(Allocate|Free).*Heap.*\w*",
       rb"ntdll\.dll!RtlpExecuteHandlerForException",
       rb"ntdll\.dll!RtlpFreeDebugInfo",
-      rb"ntdll\.dll!RtlpHandleInvalidUserCallTarget",
       rb"ntdll\.dll!RtlRaiseException",
       rb"ntdll\.dll!RtlReportCriticalFailure",
-      rb"ntdll\.dll!RtlUserThreadStart$filt$0",
-      rb"ntdll\.dll!vDbgPrintExWithPrefixInternal",
+      rb"ntdll\.dll!RtlUserThreadStart\$filt\$0",
+      rb"ntdll\.dll!v?DbgPrint.*",
     ],
   ),
   # Breakpoint -> HeapCorrupt
