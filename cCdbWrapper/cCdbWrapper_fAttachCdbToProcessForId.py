8b603e3e8988f5427f10c69bd4d087396630717d
SkyLined <1959110+SkyLined@users.noreply.github.com>
2025-05-22 11:57:18 +0200

    Add new way that cdb.exe informs of debugging failed

--- cCdbWrapper/cCdbWrapper_fAttachCdbToProcessForId.py
+++ cCdbWrapper/cCdbWrapper_fAttachCdbToProcessForId.py
@@ -7,6 +7,15 @@ def cCdbWrapper_fAttachCdbToProcessForId(oCdbWrapper, uProcessId):
     sb0Comment = b"Attach to process %d" % uProcessId,
   );
   if asbAttachToProcessOutput == [
+    b"Cannot debug pid %d, Win32 error 0n5" % uProcessId,
+    b'    "Access is denied."',
+    b"Unable to initialize target, Win32 error 0n5",
+  ]:
+    sMessage = "Unable to attach to process %d/0x%X because it is being debugged or has a higher integrity level." % (uProcessId, uProcessId);
+    assert oCdbWrapper.fbFireCallbacks("Failed to debug application", sMessage), \
+        sMessage;
+    oCdbWrapper.fStop();
+  elif asbAttachToProcessOutput == [
     b"Cannot debug pid %d, Win32 error 0n87" % uProcessId,
     b'    "The parameter is incorrect."',
     b"Unable to initialize target, Win32 error 0n87",
@@ -20,7 +29,17 @@ def cCdbWrapper_fAttachCdbToProcessForId(oCdbWrapper, uProcessId):
     b'    "Access is denied."',
     b"Unable to initialize target, Win32 error 0n5",
   ]:
+    # This is an older message that I think is not output by newer versions of
+    # cdb.exe, which output the message below this instead.
     sMessage = "Unable to attach to process %d/0x%X because it is being debugged or has a higher integrity level." % (uProcessId, uProcessId);
+    assert oCdbWrapper.fbFireCallbacks("Failed to debug application", sMessage), \
+        sMessage;
+    oCdbWrapper.fStop();  elif asbAttachToProcessOutput == [
+    b"Cannot debug pid %d, NTSTATUS 0xC0000048"% uProcessId,
+    b'    "The process that you are attempting to attach to is already being debugged. Only one debugger can be invasively attached to a process at a time. A non-invasive attach is still possible when another debugger is attached."',
+    b'Unable to initialize target, NTSTATUS 0xC0000048',
+  ]:
+    sMessage = "Unable to attach to process %d/0x%X because it is already being debugged." % (uProcessId, uProcessId);
     assert oCdbWrapper.fbFireCallbacks("Failed to debug application", sMessage), \
         sMessage;
     oCdbWrapper.fStop();
